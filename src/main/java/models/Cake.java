f8716f147ceee9734c77a703b9cb1690e463f38a
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-04-03 09:25:06 -0400

    added cake and pie accessors and mutators

--- src/main/java/models/Cake.java
+++ src/main/java/models/Cake.java
@@ -6,4 +6,44 @@ public class Cake {
     int tiers;
     int qty;
     float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public int getTiers() {
+        return tiers;
+    }
+
+    public void setTiers(int tiers) {
+        this.tiers = tiers;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/CakeTest.java
+++ src/test/java/models/CakeTest.java
@@ -1,4 +1,59 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CakeTest {
+    Cake cake;
+
+    @BeforeEach
+    public void before(){
+        cake = new Cake();
+    }
+
+    @Test
+    public void testSetFlavor() {
+        String expected = "Black forest";
+
+        cake.setFlavor(expected);
+
+        Assertions.assertEquals(expected, cake.getFlavor());
+    }
+
+    @Test
+    public void testSetId() {
+        int expected = 1;
+
+        cake.setId(expected);
+
+        Assertions.assertEquals(expected, cake.getId());
+    }
+
+    @Test
+    public void testSetTiers() {
+        int expected = 1;
+
+        cake.setTiers(expected);
+
+        Assertions.assertEquals(expected, cake.getTiers());
+    }
+
+    @Test
+    public void testSetQty() {
+        int expected = 1;
+
+        cake.setQty(expected);
+
+        Assertions.assertEquals(expected, cake.getQty());
+    }
+
+    @Test
+    public void testSetPrice() {
+        int expected = 1;
+
+        cake.setPrice(expected);
+
+        Assertions.assertEquals(expected, cake.getPrice());
+    }
 }
